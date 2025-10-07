06de0900780e94b15c395040827fc415fb74221d
AmuKanda99 <amugame99@gmail.com>
2025-10-07 15:57:21 +0100

    Add Amu's page to multi-page

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -3,10 +3,12 @@ import app_pages.multi_page as mp
 
 from app_pages.page1 import page1
 from app_pages.page2 import page2
+from app_pages.amu_page import amu_page
 
 app = mp.Multipage("My First Multipage App")
 
 app.add_page("Page 1", page1)
 app.add_page("Page 2", page2)
+app.add_page("Amu's Page", amu_page)
 
 app.run()
