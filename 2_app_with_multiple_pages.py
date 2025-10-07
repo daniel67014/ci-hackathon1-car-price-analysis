0713f767dbc8c6f74685fb377b548d922998a5e1
daniel9141141694 <lvyf3k8x@duck.com>
2025-10-07 23:09:51 +0100

    fix amu_page add pages to streamlit app

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -3,10 +3,14 @@ import app_pages.multi_page as mp
 
 from app_pages.page1 import page1
 from app_pages.page2 import page2
+from app_pages.amu_page import amu_page
+from app_pages.dan_page import dan_page
 
-app = mp.Multipage("My First Multipage App")
+app = mp.Multipage("MAD Ken Dash - Car Price Analysis")
 
 app.add_page("Page 1", page1)
 app.add_page("Page 2", page2)
+app.add_page("amu", amu_page)
+app.add_page("dan", dan_page)
 
 app.run()
--- app_pages/amu_page.py
+++ app_pages/amu_page.py
@@ -2,11 +2,12 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("C:\\Users\\admin\\Documents\\vscode-projects\\ci-hackaton1-car-price-analysis\\ci-hackathon1-car-price-analysis\\Data\\KaggleCarArchive\\CarPrice_Working.csv")
+def amu_page():
+    df = pd.read_csv("Data/KaggleCarArchive/CarPrice_Working.csv")
 
-st.title("This is Amu's page")
-st.write("This is Amu's page of the multipage app!")
+    st.title("This is Amu's page")
+    st.write("This is Amu's page of the multipage app!")
 
-fig = px.box(df, x="drivewheel", y="horsepower", color="drivewheel",
-             title="Horsepower Distribution by Drivewheel Type")
-fig.show()
+    fig = px.box(df, x="drivewheel", y="horsepower", color="drivewheel",
+                title="Horsepower Distribution by Drivewheel Type")
+    st.plotly_chart(fig)
