6e91132bbba564dd4d60c59bede84b1190f8d1d6
Eddie Calmo <kenbarron@gmail.com>
2025-10-08 15:57:25 +0100

    MODIFY - columns on brand explorer

--- app_pages/brand_explorer.py
+++ app_pages/brand_explorer.py
@@ -3,7 +3,10 @@ import plotly.express as px
 import pandas as pd
 import os
 
+
 def brand_explorer():
+
+    col1, col2 = st.columns(2)
     # Load dataset from session state
     df_CarPriceCSV = st.session_state.df
     df_CarPriceCSV['count'] = 1
@@ -20,22 +23,29 @@ def brand_explorer():
     brand_options = ["All Brands"] + brand_data['carBrand'].tolist()
     selected_brand = st.selectbox("🔍 Choose a brand to explore:", brand_options)
 
-    # Display chart based on selection
     if selected_brand == "All Brands":
         st.subheader("Car Brands Overview")
-        brand_fig = px.pie(brand_data, names='carBrand', values='count', title="Car Brands")
-        st.plotly_chart(brand_fig, use_container_width=True)
     else:
         st.subheader(f"{selected_brand} Models Breakdown")
-        model_data = (
-            df_CarPriceCSV[df_CarPriceCSV['carBrand'] == selected_brand]
-            .groupby('carModel', as_index=False)['count'].sum()
-        )
-        if model_data.empty:
-            st.warning(f"No models found for brand: {selected_brand}")
-        else:
-            model_fig = px.pie(model_data, names='carModel', values='count', title=f"{selected_brand} Models")
-            st.plotly_chart(model_fig, use_container_width=True)
 
-    # Display brand distribution image
-    st.image("images/panda_pie.jpg", caption="Brand distribution", use_container_width=True)
+    with col1:
+
+        # Display chart based on selection
+        if selected_brand == "All Brands":
+            brand_fig = px.pie(brand_data, names='carBrand', values='count', title="Car Brands")
+            st.plotly_chart(brand_fig, use_container_width=True)
+        else:
+            model_data = (
+                df_CarPriceCSV[df_CarPriceCSV['carBrand'] == selected_brand]
+                .groupby('carModel', as_index=False)['count'].sum()
+            )
+            if model_data.empty:
+                st.warning(f"No models found for brand: {selected_brand}")
+            else:
+                model_fig = px.pie(model_data, names='carModel', values='count', title=f"{selected_brand} Models")
+                st.plotly_chart(model_fig, use_container_width=True)
+
+    with col2:
+
+        # Display brand distribution image
+        st.image("images/panda_pie.jpg", caption="Brand distribution", use_container_width=True)
