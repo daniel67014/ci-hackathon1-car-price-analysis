ceafc1a701f79473b0a79846609af663634aa3cd
mr-aidid <mr.aidid@hotmail.co.uk>
2025-10-08 15:31:54 +0100

    changed name to no underscores

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -17,14 +17,8 @@ app = mp.Multipage("MAD Ken Dash - Car Price Analysis")
 
 app.add_page("Car Price Analysis", main_page)
 app.add_page("Price vs Brand", PriceVsBrand)
-<<<<<<< HEAD
-app.add_page("PricevsEnginesize", PricevsEnginesize)
-app.add_page("amu", amu_page)
-app.add_page("dan", dan_page)
-=======
 app.add_page("Price vs. Enginesize", Price_vs_Enginesize)
 app.add_page("Horsepower Distribution", amu_page)
 app.add_page("Brand Explorer", brand_explorer)
->>>>>>> a80da571561fb82a54a19d1a057e2de2200794be
 
 app.run()
