d47c36b0f490bb628e8b8877d017114defe68c8d
mr-aidid <mr.aidid@hotmail.co.uk>
2025-10-08 11:55:28 +0100

    add Project-vs-Enginesize.py

---
+++ app_pages/Price-vs-Enginesize.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df_CarPriceCSV = pd.read_csv(r'Data\KaggleCarArchive\CarPrice_Working.csv')
+
+def Price_vs_Enginesize():
+    st.title("Page 1")
+    st.write("Welcome to Page 1 of the multipage app!")
+    
+    # Create a bar chart visualization for Engine Size vs Price
+    plt.figure(figsize=(10, 6))
+    plt.bar(df_CarPriceCSV['enginesize'], df_CarPriceCSV['price'], color='blue')
+    plt.xlabel('Engine Size')
+    plt.ylabel('Price')
+    plt.title('Engine Size vs Price')
+    plt.tight_layout()
+    st.pyplot(plt)
+
+
+
